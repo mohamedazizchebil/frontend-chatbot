452dc8b3270b66fec01eee3cf02f743122c54465
mohamedazizchebil <chebilaziz461@gmail.com>
2025-07-09 11:57:24 +0100

    fix: update image source paths to use absolute paths for consistency

--- src/app/components/ChatHeader.jsx
+++ src/app/components/ChatHeader.jsx
@@ -4,7 +4,7 @@ export default function ChatHeader() {
   return (
     <div className="bg-blue-500 text-white px-3 py-2 rounded-t-lg flex items-center gap-2">
       <img
-        src="chatbot.jpg"
+        src="/chatbot.jpg"
         alt="Logo Aidea"
         width={26}
         height={26}
--- src/app/components/Message.jsx
+++ src/app/components/Message.jsx
@@ -22,7 +22,7 @@ export default function Message({ type = "text", content, role = "bot" }) {
       {!isUser && (
         <div className="flex items-center gap-2 mb-1">
           <img
-            src="chatbot.jpg"
+            src="/chatbot.jpg"
             alt="Avatar bot"
             width={24}
             height={24}
